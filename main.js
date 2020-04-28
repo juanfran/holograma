12c2ff1b12e57d801c23889166f7808d7968d98f
Juanfran <juanfran.ag@gmail.com>
2020-04-28 23:16:44 +0200

    wip

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var net;
-var scene = new THREE.Scene();
 var renderer;
 var particles;
 var camera;
 var aframeScene;
-var shaderMaterialParticles;
 var textureOriginal = new THREE.Texture();
 var textureDepthMap = new THREE.Texture();
 var imageCapture;
+var fragmentShader;
+var vertexshaderParticle;
 
 function capture() {
   imageCapture.grabFrame().then((imageBitmap) => {
@@ -45,7 +45,7 @@ function createTextureAndParticlesFromSegmentation(seg) {
   }
 
   var size = seg.width * seg.height;
-  var data = new Uint8Array( 3 * size );
+  var data = new Uint8Array(3 * size);
   subsamplingFactor = 4;
   var w = seg.width;
   var h = seg.height;
@@ -53,20 +53,18 @@ function createTextureAndParticlesFromSegmentation(seg) {
   var positions = [];
   var uvs = [];
   var u,v;
-  var nbParticles = 0;
   var nAdded = 0;
 
-  for ( var i = 0; i < size; i ++ ) {
+  for (var i = 0; i < size; i ++) {
     // Texture part
     var stride = i * 3;
-    data[ stride ] = seg.data[i] * 255;
-    data[ stride + 1 ] = seg.data[i];
-    data[ stride + 2 ] = seg.data[i];
+    data[stride] = seg.data[i] * 255;
+    data[stride + 1] = seg.data[i];
+    data[stride + 2] = seg.data[i];
 
     // Particles part. We test if People or not to create a particle
     if(seg.data[i] > 0){
         if(nAdded % subsamplingFactor == 0){
-            nbParticles++;
             u = (i % w ) / w;
             v = Math.floor(i / w) / h;
             positions.push(Math.random(), Math.random(), Math.random());
@@ -76,28 +74,53 @@ function createTextureAndParticlesFromSegmentation(seg) {
     }
   }
 
+  const worldPosition = new THREE.Vector3();
+  camera.object3D.getWorldPosition(worldPosition);
+  const worldQuaternion = new THREE.Quaternion();
+  camera.object3D.getWorldQuaternion(worldQuaternion);
+  const worldHeading = new THREE.Vector3();
+  camera.object3D.getWorldDirection(worldHeading);
+
   // Part particles
   geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
   geometry.setAttribute( 'uvs', new THREE.Float32BufferAttribute( uvs, 2 ) );
 
-  particles = new THREE.Points( geometry, shaderMaterialParticles );
+  const shaderMaterialParticles = new THREE.ShaderMaterial( {
+    uniforms: {
+      optionMode: {value: 1},
+      userPos: {value: worldPosition},
+      width: {value: 1},
+      height: {value: 1},
+      time: {value: time},
+      originalImage: { value: textureOriginal },
+      depthMap: { value: textureDepthMap}
+    },
+    vertexShader: vertexshaderParticle,
+    fragmentShader: fragmentShader,
+    depthTest: false,
+    transparent: true,
+    vertexColors: true
+  });
+
+  particles = new THREE.Points(geometry, shaderMaterialParticles);
   particles.material.uniforms.originalImage.value = textureOriginal;
   particles.position.z = -4;
+  particles.position.y = 1;
   particles.frustumCulled = false;
-  particles.material.uniforms.width.value = w ;
-  particles.material.uniforms.height.value = h ;
+  particles.material.uniforms.width.value = w;
+  particles.material.uniforms.height.value = h;
 
   // Part texture
-  textureSegmentation = new THREE.DataTexture( data, seg.width, seg.height, THREE.RGBFormat );
+  textureSegmentation = new THREE.DataTexture(data, seg.width, seg.height, THREE.RGBFormat);
   textureSegmentation.flipY = true;
   time = 0;
-  particles.material.uniforms.time.value = time ;
+  particles.material.uniforms.time.value = time;
   particles.material.uniforms.depthMap.value = textureSegmentation;
 
-  aframeScene.setObject3D('particle-system', particles);
-/*
-  scene.add( particles );
-  animate(); */
+  var entityEl = document.createElement('a-entity');
+  entityEl.setObject3D('particle-system', particles);
+
+  aframeScene.appendChild(entityEl);
 }
 
 function init() {
@@ -106,34 +129,15 @@ function init() {
 
   document.querySelector('a-scene').addEventListener('loaded', async () => {
     const responseFragmentShader = await fetch('./fragmentshaderParticle');
-    const fragmentShader = await responseFragmentShader.text();
+    fragmentShader = await responseFragmentShader.text();
 
     const responseVertexshaderParticle = await fetch('./vertexshaderParticle');
-    const vertexshaderParticle = await responseVertexshaderParticle.text();
+    vertexshaderParticle = await responseVertexshaderParticle.text();
 
     aframeScene = document.querySelector('a-scene');
 
     renderer = aframeScene.renderer;
-    camera = document.querySelector('#camera').components.camera.camera;
-
-    const uniforms = {
-      optionMode: {value: 1},
-      userPos: {value: camera.position},
-      width: {value: 1},
-      height: {value: 1},
-      time: {value: time},
-      originalImage: { value: textureOriginal },
-      depthMap: { value: textureDepthMap}
-    };
-
-    shaderMaterialParticles = new THREE.ShaderMaterial( {
-      uniforms: uniforms,
-      vertexShader: vertexshaderParticle,
-      fragmentShader: fragmentShader,
-      depthTest: false,
-      transparent: true,
-      vertexColors: true
-    });
+    camera = document.querySelector('#camera');
 
     aframeScene.addEventListener('click', () => {
       if (!imageCapture) {
